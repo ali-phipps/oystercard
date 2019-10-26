6fd1872979efdfd8321130b3798a45cadc175589
Alistair Phipps <alistair.phipps@johnlewis.co.uk>
2019-10-26 16:13:37 +0100

    experimentation with dependency injection

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,20 +1,25 @@
 class JourneyLog
   attr_reader :trip_records
 
-  def initialize
+  def initialize(journey = Journey.new)
     @trip_records = []
+    @journey = journey
 
   end
 
   def start_journey(origin)
-    @origin = origin
+    # @journey = @journey_class.n
+    @journey.start_station(origin)
+    # @origin = origin
   end
 
   def end_journey(destination)
-    @current_journey = {}
-    @current_journey[:origin] = @origin
-    @current_journey[:destination] = destination
-    @trip_records << @current_journey
-    @current_journey
+    # @current_journey = {}
+    # @current_journey[:origin] = origin
+    # @current_journey[:destination] = destination
+    @journey.end_station(destination)
+    @trip_records << @journey
+    @journey
   end
+
 end
--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -1,29 +1,48 @@
 require 'journey_log'
 
 describe JourneyLog do
-  subject(:log) {JourneyLog.new}
+
   let(:origin) {double :station}
   let(:destination) {double :station}
+  let(:journey) { double :journey}
+  subject(:log) {JourneyLog.new(journey)}
+
     context "First time card used" do
+    before do
+      allow(journey).to receive(:start_station) {{origin: origin} }
+    end
       it "on start journey" do
+
         log.start_journey(origin)
         expect(log.trip_records).to be_empty
       end
       it "on end journey"  do
+        allow(journey).to receive(:end_station) {{destination: destination} }
         log.start_journey(origin)
         log.end_journey(destination)
-        expect(log.trip_records).to eq ([{ origin: origin, destination: destination }])
+        expect(log.trip_records).to eq ([journey])
       end
     end
+
 describe "Start Journey" do
   it 'sets origin' do
-    expect(log.start_journey(origin)).to eq(origin)
+    allow(journey).to receive(:start_station) {{origin: origin} }
+    expect(log.start_journey(origin)).to eq({origin: origin})
   end
 end
+
 describe 'End journey' do
   it 'sets destination and updates log' do
+    allow(journey).to receive(:start_station) {{origin: origin} }
+    allow(journey).to receive(:end_station) {{destination: destination} }
     log.start_journey(origin)
-    expect(log.end_journey(destination)).to eq({origin: origin, destination: destination})
+    expect(log.end_journey(destination)).to eq(journey)
   end
 end
+
+  # describe 'update journey log' do
+  #   it 'writes' do
+  #     expect(log.update_log(origin, destination)).to eq ([{ origin: origin, destination: destination }])
+  #   end
+
 end
